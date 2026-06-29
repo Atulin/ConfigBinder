2610d6ef37a0334c31a6fdf58d1f1cdbaf5b90e7
Atulin <11233299+Atulin@users.noreply.github.com>
2026-06-30 01:28:25 +0200

    Init

---
+++ ConfigBinder.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ConfigBinder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration["SomeConfig:SomeString"] = "Hello World!";
+builder.Configuration["SomeConfig:SomeInt"] = "42";
+
+
+[BindConfig("SomeConfig")]
+internal sealed class SomeConfig
+{
+	[Required]
+	public required string SomeString { get; init; }
+	
+	[Range(0, 100)]
+	public required int SomeInt { get; init; }
+}
---
+++ ConfigBinder/ConfigModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ConfigBinder;
+
+internal sealed record ConfigModel(
+	string FullyQualifiedName,
+	string TypeName,
+	string SectionName
+)
+{
+	public string SafeName => FullyQualifiedName
+		.Replace("global::", "")
+		.Replace('.', '_')
+		.Replace('<', '_')
+		.Replace('>', '_')
+		.Replace(',', '_')
+		.Replace(' ', '_');
+}
